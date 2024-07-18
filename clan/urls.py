9d2f07bc85770533da07e48981720cd056043e8e
BloggingKIng <hzuaifablogger.contact@gmail.com>
2024-07-18 19:52:31 +0500

    Made different pages for current war current war and overall performance.
    Avoided the refreshing that used to happen everytime
    when the page loads

--- clan/urls.py
+++ clan/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('overview/', views.members_page, name='clan_members_page'),
     path('refresh_stars/', views.refresh_stars, name='refresh_stars'),
+    path('current_war_details/', views.current_war, name='current_war_details'),
 ]
--- clan/views.py
+++ clan/views.py
@@ -7,6 +7,10 @@ import os
 api_key = os.getenv('API_KEY')
 clan_tag = "2G8YV0J02"
 def members_page(request):
+    members_list = ClanMembers.objects.all()
+    return render(request, 'clan/clan_members_details.html', {'members': members_list})
+
+def refresher():
     fetch_members = requests.get(f"https://api.clashofclans.com/v1/clans/%23{clan_tag}/members", headers={'Authorization': f"Bearer {api_key}"})
     members = fetch_members.json()
     for member in members['items']:
@@ -15,14 +19,20 @@ def members_page(request):
         if not member_exist:
             new_member = ClanMembers(member_tag=member['tag'], member_name=member['name'], trophies=member['trophies'], tracked_stars=0)
             new_member.save()
-    members_list = ClanMembers.objects.all()
-    
-    return render(request, 'clan/clan_members_details.html', {'members': members_list})
+        else:
+            m = ClanMembers.objects.filter(member_tag=tag).first()
+            m.trophies = member['trophies']
+            m.save()
 
-def refresh_stars(request):
     war_endpoint = f"https://api.clashofclans.com/v1/clans/%23{clan_tag}/currentwar"
     war = requests.get(war_endpoint, headers={'Authorization': f"Bearer {api_key}"})
     war = war.json()
+
+    clan_members = ClanMembers.objects.all()
+    for member in clan_members:
+        member.participating_in_current_war = False
+        member.save()
+    
     if war['state'] == "inWar":
         for member in war['clan']['members']:
             tag = member['tag']
@@ -56,5 +66,43 @@ def refresh_stars(request):
                 m.attacked_in_current_war = False
                 m.stars_in_current_war = 0
                 m.save()
+            m.participating_in_current_war = True
+            m.save()
+    elif war['state'] == "preparation":
+        for member in war['clan']['members']:
+            tag = member['tag']
+            m = ClanMembers.objects.filter(member_tag=tag).first()
+            if not m:
+                player_details = requests.get(f"https://api.clashofclans.com/v1/players/%23{tag}", headers={'Authorization': f"Bearer {api_key}"})
+                if player_details.status_code != 200:
+                    print(player_details.json())
+                    continue
+                player_details = player_details.json()
+                m = ClanMembers.objects.create(member_tag=tag, member_name=player_details['name'], trophies=player_details['trophies'], tracked_stars=0, participating_in_current_war=False)
+            m.participating_in_current_war = True
+            m.attacked_in_current_war = False
+            m.stars_in_current_war = 0
+            m.save()
 
-    return redirect('clan_members_page')
+def refresh_stars(request):
+    refresher()
+    return redirect(request.META.get('HTTP_REFERER'))
+
+
+def current_war(request):
+    members = ClanMembers.objects.filter(participating_in_current_war=True)
+    total_attacks = 0
+    total_stars = 0
+    attacking_members = 0
+    for member in members:
+        total_attacks += member.stars_in_current_war
+        total_stars += member.tracked_stars
+        if member.attacked_in_current_war:
+            attacking_members += 1
+    context = {
+        'total_attacks': total_attacks,
+        'total_stars': total_stars,
+        'attacking_members': attacking_members,
+        'members': members
+    }
+    return render(request, 'clan/current_war_details.html', context)
