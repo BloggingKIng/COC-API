e166493c5e212ac9432a009679550b38e406af1b
BloggingKIng <hzuaifablogger.contact@gmail.com>
2024-07-19 14:47:06 +0500

    "Fixed errors"

--- WarManager/settings.py
+++ WarManager/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'clashcrafters.azurewebsites.net',
+    '127.0.0.1'
 ]
 
 
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT =   '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
